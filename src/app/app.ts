5cbb4a11a4767361085ecb12473a4356193fe161
JFC <josefreiriacastro@hotmail.es>
2026-04-15 21:11:50 +0200

    Añado sweetalert

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,44 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('teslo-shop');
+
+
+
+  ngAfterViewInit() {
+    let timerInterval: number | undefined;
+
+    Swal.fire({
+      title: "¡Despertando al servidor!",
+      html: "Puede que el servidor esté dormido <br> Tardará unos <b></b> segundos.",
+      timer: 50000,
+      timerProgressBar: true,
+
+      showConfirmButton: true,
+      confirmButtonText: "Cerrar ahora",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+
+      didOpen: () => {
+        const timer = Swal.getPopup()!.querySelector("b");
+
+        timerInterval = window.setInterval(() => {
+          const milisegundosRestantes = Swal.getTimerLeft();
+          const segundosRestantes = Math.ceil(milisegundosRestantes! / 1000);
+
+          timer!.textContent = `${segundosRestantes}`;
+        }, 100);
+      },
+
+      willClose: () => {
+        clearInterval(timerInterval);
+      }
+
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log("Cerrado manualmente");
+      } else if (result.dismiss === Swal.DismissReason.timer) {
+        console.log("Cerrado por el timer");
+      }
+    });
+  }
 }
